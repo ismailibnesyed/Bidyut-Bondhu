ebd01cfbd9d963ea0523b539f736c8de3d67fa4d
ismailibnesyed <iammdismailhossain.com>
2026-09-25 23:37:15 +0600

    update baseUrl

--- backend/main.py
+++ backend/main.py
@@ -29,8 +29,7 @@ app = FastAPI(title="Load Shedding & Power Outage Management System")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
