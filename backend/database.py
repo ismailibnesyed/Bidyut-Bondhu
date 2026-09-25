b8dae79c5320a7570e273cd184db06c44973b35f
ismailibnesyed <iammdismailhossain.com>
2026-09-25 23:15:35 +0600

    update database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = 'postgresql://postgres.vcbcslbsrpnphjdgkogy:HossainIsmailMd@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres'
+DATABASE_URL = 'postgresql+psycopg2://postgres.vcbcslbsrpnphjdgkogy:HossainIsmailMd@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine )
